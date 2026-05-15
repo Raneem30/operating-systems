0f252af55710fc1cca7dc667da5576f8dde25ddb
Ola Ahmed <o.ahmed2326@nu.edu.eg>
2026-05-16 01:25:44 +0300

    Add files via upload

---
+++ xv6/kernel/filelog (1).h
@@ -0,0 +1,28 @@
+// kernel/filelog.h
+ 
+#ifndef FILELOG_H
+#define FILELOG_H
+ 
+#define MAX_LOGS     512
+#define MAX_FILENAME 128
+#define MAX_PROCNAME 16
+ 
+#define ACTION_OPEN  0
+#define ACTION_READ  1
+#define ACTION_WRITE 2
+#define ACTION_CLOSE 3
+ 
+struct log_entry {
+    int  pid;
+    char proc_name[MAX_PROCNAME];
+    char file_name[MAX_FILENAME];
+    int  action;
+    int  tick;
+};
+ 
+// Functions defined in filelog.c
+void filelog_init(void);
+void filelog_add(int action, char *filename);
+int  filelog_get(struct log_entry *buf, int max_entries);
+ 
+#endif
