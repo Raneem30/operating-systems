8d23241f54795e3374912fa83708b2118b0bf8d2
Ola Ahmed <o.ahmed2326@nu.edu.eg>
2026-05-15 17:35:22 +0300

    Update filelog.h

--- filelog.h
+++ filelog.h
@@ -3,14 +3,14 @@
 #ifndef FILELOG_H
 #define FILELOG_H
 
-#define MAX_LOGS [512]    
-#define MAX_FILENAME [128]  
-#define MAX_PROCNAME [16]
+#define MAX_LOGS 512
+#define MAX_FILENAME 128
+#define MAX_PROCNAME 16
 
-#define ACTION_OPEN  [0]
-#define ACTION_READ  [1]
-#define ACTION_WRITE [2]
-#define ACTION_CLOSE [3]
+#define ACTION_OPEN  0
+#define ACTION_READ  1
+#define ACTION_WRITE 2
+#define ACTION_CLOSE 3
 
 struct log_entry {
     int  pid;                    
